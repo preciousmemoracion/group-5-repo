207be8c24b8b9d9a454485172b4f64a3be236a5f
preciousmemoracion <preciousmemoracion>
2026-05-06 09:43:18 +0800

    ghbgh

--- db.php
+++ db.php
@@ -1,7 +1,17 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "crud_db");
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$database   = "crud_db";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Set charset (important for proper text handling)
+$conn->set_charset("utf8mb4");
 ?>
--- index.php
+++ index.php
@@ -27,18 +27,26 @@
 <?php
 $result = $conn->query("SELECT * FROM users");
 
-while($row = $result->fetch_assoc()) {
+if ($result && $result->num_rows > 0):
+    while($row = $result->fetch_assoc()):
 ?>
 <tr>
-    <td><?= $row['id']; ?></td>
-    <td><?= $row['name']; ?></td>
-    <td><?= $row['email']; ?></td>
+    <td><?= htmlspecialchars($row['id']); ?></td>
+    <td><?= htmlspecialchars($row['name']); ?></td>
+    <td><?= htmlspecialchars($row['email']); ?></td>
     <td>
         <a href="edit.php?id=<?= $row['id']; ?>">Edit</a> |
-        <a href="delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Delete?')">Delete</a>
+        <a href="delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Delete this user?')">Delete</a>
     </td>
 </tr>
-<?php } ?>
+<?php 
+    endwhile;
+else:
+?>
+<tr>
+    <td colspan="4">No users found</td>
+</tr>
+<?php endif; ?>
 
 </table>
 
